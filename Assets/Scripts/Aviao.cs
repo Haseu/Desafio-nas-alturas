db8a1162c203e5ad28fe84297de34f114377664c
André Menutole <andremenutole@gmail.com>
2020-02-21 21:44:41 -0300

    Adicionado obstaculos e movimentacao do aviao

---
+++ Assets/Scripts/Aviao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aviao : MonoBehaviour
+{
+    Rigidbody2D fisica;
+
+    private void Awake() {
+        this.fisica = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            this.inmpulsionar();
+        }
+    }
+
+    private void inmpulsionar()
+    {
+        fisica.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+    }
+}
